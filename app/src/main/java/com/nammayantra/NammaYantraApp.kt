a3298d083159a5e355a47603a5c76d1cc701fa56
Prajwal <144578670+M-Prajwal-6@users.noreply.github.com>
2026-05-12 21:02:12 +0530

    Update Cloudinary config with placeholder values
    Replaced Cloudinary configuration values with placeholders.

--- app/src/main/java/com/nammayantra/NammaYantraApp.kt
+++ app/src/main/java/com/nammayantra/NammaYantraApp.kt
@@ -13,9 +13,9 @@ class NammaYantraApp : Application() {
         
         // Initialize Cloudinary once for the whole app
         val config = mapOf(
-            "cloud_name" to "dzf6d8d8m",
-            "api_key" to "875511919113518",
-            "api_secret" to "nwoptau0M95tHYlusXlnUs1csEs"
+            "cloud_name" to " CLOUD NAME ",
+            "api_key" to " API KEY ",
+            "api_secret" to " SECRET KEY "
         )
         MediaManager.init(this, config)
     }
